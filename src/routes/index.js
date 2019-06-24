974aa7467288db4fc8d33fe527aa07781a52edfd
Igor Souza <igor.souza@wunderman.com>
2019-06-24 19:48:43 -0300

    Adiciona footer com conexao aos states do redux

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import Footer from '../components/Footer';
+
 import Main from '../pages/main';
 
 const Routes = () => (
   <BrowserRouter>
-    <Switch>
-      <Route exact path="/" component={Main} />
-    </Switch>
+    <>
+      <Switch>
+        <Route exact path="/" component={Main} />
+      </Switch>
+      <Footer />
+    </>
   </BrowserRouter>
 );
 
